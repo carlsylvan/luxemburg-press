3edda7d4e5df5a4fa5225ceddab4da50ad779029
carlsylvan <carlsylvan@gmail.com>
2024-01-02 16:47:15 +0100

    index ts started express

---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+// import express from "express";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT;
+
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Hello, TypeScript Express!');
+// });
+  
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
