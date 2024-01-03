edc25a5d8b3fc4ee1ab4d4824aece6cba00729ee
carlsylvan <carlsylvan@gmail.com>
2024-01-03 14:18:24 +0100

    header css and import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div> */}
+      <Header></Header>
       <h1>Luxemburg Press</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import "./header.css"
+
+export default function Header() {
+
+    return(<div className="header">Header</div>)
+}
