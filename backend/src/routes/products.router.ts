2171a7ddc1d5083f314b07ec8799834f6a860c63
carlsylvan <carlsylvan@gmail.com>
2024-01-04 12:35:48 +0100

    added POST PUT DELETE to products router

--- backend/src/routes/products.router.ts
+++ backend/src/routes/products.router.ts
@@ -55,10 +55,60 @@ productsRouter.get("/:id", async (req: Request, res: Response) => {
     }
 });
 
-
-
 // POST
+
+productsRouter.post("/", async (req: Request, res: Response) => {
+    try {
+        const newProduct = req.body as Product;
+        const result = await collections.products?.insertOne(newProduct);
+
+        result
+            ? res.status(201).send(`Successfully created a new game with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new game.");
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error.message);
+    }
+});
 
 // PUT
 
+productsRouter.put("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const updatedGame: Product = req.body as Product;
+        const query = { _id: new ObjectId(id) };
+      
+        const result = await collections.products?.updateOne(query, { $set: updatedGame });
+
+        result
+            ? res.status(200).send(`Successfully updated game with id ${id}`)
+            : res.status(304).send(`Game with id: ${id} not updated`);
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
+
 // DELETE
+
+productsRouter.delete("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const query = { _id: new ObjectId(id) };
+        const result = await collections.products?.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Successfully removed game with id ${id}`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove game with id ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Game with id ${id} does not exist`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
