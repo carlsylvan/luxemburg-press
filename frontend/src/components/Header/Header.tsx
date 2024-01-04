e01061c126a166de7c745081490f6b179be8a5e9
carlsylvan <carlsylvan@gmail.com>
2024-01-04 11:18:17 +0100

    icons navbar

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,25 @@
 import "./header.css"
+import cartIcon from '../../assets/cart-shopping-solid.svg'
+import storeIcon from '../../assets/store-solid.svg'
+import infoIcon from '../../assets/circle-info-solid.svg'
 
 export default function Header() {
 
     return(<div className="header">
         <div className="nav">
         <li className="nav-list">
-            <ul className="nav-list-items">Info</ul>
-            <ul className="nav-list-items">Köp</ul>
-            <ul className="nav-list-items">Korg</ul>
+            <ul className="nav-list-item">
+                Info
+                <img src={infoIcon}></img>
+                </ul>
+            <ul className="nav-list-item">
+                Köp
+                <img src={storeIcon}></img>
+            </ul>
+            <ul className="nav-list-item">
+                Korg
+                <img src={cartIcon}></img>
+            </ul>
         </li>
         </div>
     </div>)
