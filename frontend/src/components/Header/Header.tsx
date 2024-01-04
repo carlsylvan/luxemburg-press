8d4cafe5cc21af698703460776cf470ce8e21bdc
carlsylvan <carlsylvan@gmail.com>
2024-01-04 11:05:54 +0100

    nav list to header navbar

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,5 +2,13 @@ import "./header.css"
 
 export default function Header() {
 
-    return(<div className="header">Header</div>)
+    return(<div className="header">
+        <div className="nav">
+        <li className="nav-list">
+            <ul className="nav-list-items">Info</ul>
+            <ul className="nav-list-items">Köp</ul>
+            <ul className="nav-list-items">Korg</ul>
+        </li>
+        </div>
+    </div>)
 }
