688e3a271ad96b0ee5f07b7582e942e0532dc5fe
carlsylvan <carlsylvan@gmail.com>
2024-01-04 13:37:10 +0100

    moved dotenv file

--- backend/src/services/database.service.ts
+++ backend/src/services/database.service.ts
@@ -7,27 +7,26 @@ import * as dotenv from "dotenv";
 export const collections: { products?: mongoDB.Collection } = {}
 
 // Initialize Connection
+
 export async function connectToDatabase() {
     dotenv.config();
 
-    // Check if the environment variables are defined
-    const connectionString = process.env.DB_CONN_STRING;
+    const dbConnString = process.env.DB_CONN_STRING;
     const dbName = process.env.DB_NAME;
     const productsCollectionName = process.env.PRODUCTS_COLLECTION_NAME;
 
-    if (!connectionString || !dbName || !productsCollectionName) {
-        console.error("Database configuration is missing in environment variables");
-        return;
+    if (!dbConnString || !dbName || !productsCollectionName) {
+        throw new Error("One or more environment variables are undefined.");
     }
 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(connectionString);
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(dbConnString);
             
     await client.connect();
         
     const db: mongoDB.Db = client.db(dbName);
    
     const productsCollection: mongoDB.Collection = db.collection(productsCollectionName);
- 
+
     collections.products = productsCollection;
        
     console.log(`Successfully connected to database: ${db.databaseName} and collection: ${productsCollection.collectionName}`);
