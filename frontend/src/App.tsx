20c1481c1a9e47e4ebf35c8c311a8b1fff2cd0f0
carlsylvan <carlsylvan@gmail.com>
2024-01-04 10:38:12 +0100

    removed comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,28 +11,14 @@ function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
       <Header></Header>
       <h1>Luxemburg Press</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Du har klickat {count} gånger. Testa att klicka en gång till!
         </button>
-        <img src={goblinImg} ></img>
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p> */}
+        <img id='goblin-img' src={goblinImg} ></img>
       </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
       <Footer></Footer>
     </>
   )
