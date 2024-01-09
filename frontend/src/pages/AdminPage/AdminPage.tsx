647ebfb2fa5847605177ff0d8c6e5bad0edc8e16
carlsylvan <carlsylvan@gmail.com>
2024-01-09 10:18:02 +0100

    create product form

--- frontend/src/pages/AdminPage/AdminPage.tsx
+++ frontend/src/pages/AdminPage/AdminPage.tsx
@@ -8,9 +8,37 @@ export default function AdminPage() {
     const [editProduct, setEditProduct] = useState<IProduct>();
 
     return(<div id="admin-page">
-        <div className="create-product-form">
-
-        </div>
+            <form className="create-product-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={newProduct.name}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    name="price"
+                    placeholder="Price"
+                    value={newProduct.price}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="text"
+                    name="category"
+                    placeholder="Category"
+                    value={newProduct.category}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="text"
+                    name="imgUrl"
+                    placeholder="Image URL"
+                    value={newProduct.imgUrl}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Create Product</button>
+            </form>
 
         <div className="edit-product-form">
 
