0088498e92ee0f1f12e5f6361f67309d60f799b6
carlsylvan <carlsylvan@gmail.com>
2024-01-10 09:51:01 +0100

    header css

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,25 +2,28 @@ import "./header.css"
 import cartIcon from '../../assets/cart-shopping-solid.svg'
 import storeIcon from '../../assets/store-solid.svg'
 import infoIcon from '../../assets/circle-info-solid.svg'
+import { Link } from "react-router-dom"
 
 export default function Header() {
 
     return(<div className="header">
+              <Link style={{ textDecoration: 'none' }} to="/">
               <h1>LUXEMBURG PRESS</h1>
+              </Link>
         <div className="nav">
         <li className="nav-list">
+        <Link style={{ textDecoration: 'none' }} to="/">
             <ul className="nav-list-item">
-            <a href="/">
                 Info
                 <img src={infoIcon}></img>
-            </a>
                 </ul>
+                </Link>
+                <Link style={{ textDecoration: 'none' }} to="/store">
             <ul className="nav-list-item">
-                <a href="/store">
                 Köp
                 <img src={storeIcon}></img>
-                </a>
             </ul>
+            </Link>
             <ul className="nav-list-item">
                 Korg
                 <img src={cartIcon}></img>
