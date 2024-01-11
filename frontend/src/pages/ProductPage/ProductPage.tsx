c8b6acabcfe389439c7859d00acd43f5c54d8e60
carlsylvan <carlsylvan@gmail.com>
2024-01-11 09:52:26 +0100

    Cart context and add to cart function in ProductPage

--- frontend/src/pages/ProductPage/ProductPage.tsx
+++ frontend/src/pages/ProductPage/ProductPage.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import "./productPage.css";
 import { getProductById } from "../../services/productsService";
 import { IProduct } from "../../interfaces/IProduct";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import { CartContext } from '../../contexts/CartContext';
+
 export default function ProductPage() {
     const [product, setProduct] = useState<IProduct>({
         _id: "0",
@@ -11,20 +13,47 @@ export default function ProductPage() {
         price: 0,
         category: "Kategori",
         imgUrl: ""
-      });
+    });
 
-      const { id } = useParams();
+    const { id } = useParams();
 
+    const cart = useContext(CartContext);
+    const setCart = cart.setCart;
 
     useEffect(() => {
         getProductById(id).then((product) => {
           setProduct(product);
         });
-    });
+    }, [id]);
+    const addToCart = (productToAdd: IProduct) => {
+
+      const newCartItems = [...(cart?.cart?.items || [])];
     
+      const existingCartItemIndex = newCartItems.findIndex(
+        item => item.product._id === productToAdd._id
+      );
+    
+      if (existingCartItemIndex >= 0) {
+        const existingCartItem = newCartItems[existingCartItemIndex];
+        newCartItems[existingCartItemIndex] = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1
+        };
+      } else {
+        newCartItems.push({
+          product: productToAdd,
+          quantity: 1
+        });
+      }
+    
+      setCart({ items: newCartItems });
+    };
+    
+
     return (
         <div className="page">
-                <ProductCard {...product}></ProductCard>
+            <ProductCard {...product}></ProductCard>
+            <button onClick={() => addToCart(product)}>Add to cart</button>
         </div>
     );
 }
