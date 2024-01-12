95196ed085b131335f510eb7bd33823f0d9f56c3
carlsylvan <carlsylvan@gmail.com>
2024-01-12 11:42:36 +0100

    added author to product card

--- frontend/src/components/ProductCard/ProductCard.tsx
+++ frontend/src/components/ProductCard/ProductCard.tsx
@@ -1,11 +1,11 @@
 import { IProduct } from "../../interfaces/IProduct";
 import "./productCard.css";
 
-export default function ProductCard({ title, category, price, imgUrl }: IProduct) {
+export default function ProductCard({ title, author, price, imgUrl }: IProduct) {
     return (
         <div className="product-card">
             <p>Titel: {title}</p>
-            <p>Kategori: {category}</p>
+            <p>Författare: {author}</p>
             <p>Pris: {price} kr</p>
             <img width={400} src={new URL(imgUrl, import.meta.url).href}></img>
         </div>
