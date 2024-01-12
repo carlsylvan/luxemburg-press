44c81d3a7d63bf93e88b21663d816ed904868bcb
carlsylvan <carlsylvan@gmail.com>
2024-01-12 09:16:20 +0100

    cart total cost and item links

--- frontend/src/components/Cart/Cart.tsx
+++ frontend/src/components/Cart/Cart.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
-import "./cart.css"
+import "./cart.css";
 import { Link } from "react-router-dom";
 
 export default function Cart() {
@@ -10,17 +10,25 @@ export default function Cart() {
         return <div className="cart">Your cart is empty.</div>;
     }
 
+    const totalCost = cart.items.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+
     return (
         <div className="cart">
-            {/* <button className="cart-close-button"></button> */}
             {cart.items.map((item, index) => (
                 <div key={index} className="cart-item">
-                    <div>{item.product.name}</div>
-                    <div>Quantity: {item.quantity}</div>
-                    <div>Pris: {item.product.price} kr</div>
-                    <div>Totalkostnad {item.product.price * item.quantity} kr</div>
+
+                    <Link to={`/store/${item.product._id}`}>
+                    <div id="cart-item-product-name">{item.product.name}</div>
+                    </Link>
+                    <div>Antal: {item.quantity}</div>
+                    {/* <div>Pris: {item.product.price} kr</div> */}
                 </div>
             ))}
+
+            <div id="cart-total-cost">
+                {totalCost} kr
+            </div>
+
             <Link to="/checkout">
                 <button className="cart-checkout-button">Betala</button>
             </Link>
