a9450afe64cdec9770dc67974b9befd74becebcf
carlsylvan <carlsylvan@gmail.com>
2024-01-13 13:39:48 +0100

    styling

--- frontend/src/components/ProductCard/ProductCard.tsx
+++ frontend/src/components/ProductCard/ProductCard.tsx
@@ -5,9 +5,9 @@ export default function ProductCard({ title, author, price, imgUrl }: IProduct) 
     return (
         <div className="product-card">
             {/* <marquee>Featured Product!</marquee> */}
-            <p>Title: {title}</p>
-            <p>Author: {author}</p>
-            <p>Price: {price} kr</p>
+            <p>{title}</p>
+            <p>{author}</p>
+            <p>{price} kr</p>
             <img width={400} src={new URL(imgUrl, import.meta.url).href}></img>
         </div>
     );
