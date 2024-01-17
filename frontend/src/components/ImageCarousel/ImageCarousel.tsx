f4d26b3079cf289e6020790bed50ae23a06791df
carlsylvan <carlsylvan@gmail.com>
2024-01-17 10:24:07 +0100

    img carousel

--- frontend/src/components/ImageCarousel/ImageCarousel.tsx
+++ frontend/src/components/ImageCarousel/ImageCarousel.tsx
@@ -1,41 +1,37 @@
 import { useState } from "react";
-import "./imageCarousel.css"
+import "./imageCarousel.css";
 
 interface ImageCarouselProps {
-  images: string[];
+  images: { [key: string]: string };
 }
 
 export const ImageCarousel = ({ images }: ImageCarouselProps) => {
-  const [currentIndex, setCurrentIndex] = useState(1);
-
   const imagesArray = Object.values(images);
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const goToPrevious = () => {
-    const isFirstImage = currentIndex === 0;
-    const newIndex = isFirstImage ? images.length - 1 : currentIndex - 1;
+    const newIndex = currentIndex === 0 ? imagesArray.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const goToNext = () => {
-    const isLastImage = currentIndex === images.length - 1;
-    const newIndex = isLastImage ? 0 : currentIndex + 1;
+    const newIndex = currentIndex === imagesArray.length - 1 ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
 
-
-
   return (
     <div className="image-carousel">
-      {currentIndex > 1 && (
+      {currentIndex > 0 && (
         <button onClick={goToPrevious}>&lt;</button>
       )}
-        <img 
-        src={new URL(images[currentIndex], import.meta.url).href} 
-        alt={`Slide ${currentIndex}`} 
+      <img 
+        src={new URL(imagesArray[currentIndex], import.meta.url).href} 
+        alt={`Slide ${currentIndex + 1}`} 
       />
-      {currentIndex < imagesArray.length && (
+      {currentIndex < imagesArray.length - 1 && (
         <button onClick={goToNext}>&gt;</button>
-      )}    
-      </div>
+      )}
+    </div>
   );
 };
